a50d97b73f4ab6613dffaf42fc8557779b6d52a8
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-17 16:57:33 +0100

    you're a square

---
+++ You're_a_square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class You_re_a_square
+    {
+        public static bool IsSquare(int n)
+        {
+            if (n >= 0)
+            {
+                double sqrt = Math.Sqrt((double)n);
+                if (sqrt % 1 == 0)
+                { // If it has no decimal values
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+
+
+        }
+    }
+}
+
+
+
