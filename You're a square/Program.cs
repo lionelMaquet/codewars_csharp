3527a863608689981757dcb87e01b52b4cbd0412
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-21 20:02:09 +0100

    change structure

---
+++ You're a square/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Kata
+{
+    static public void  Main()
+    {
+        int myNumber = 2;
+        IsSquare(myNumber);
+    }
+    public static bool IsSquare(int n)
+    {
+        if (n >= 0)
+        {
+            double sqrt = Math.Sqrt((double)n);
+            if (sqrt % 1 == 0)
+            { // If it has no decimal values
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
+
+    }
+}
