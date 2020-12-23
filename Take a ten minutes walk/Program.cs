0bb81fad6670e650f9cfa6209984d9caebfb710d
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-23 15:44:04 +0100

    take a ten minutes walk done

---
+++ Take a ten minutes walk/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+public class Kata
+{
+    public static bool IsValidWalk(string[] walk)
+    {
+        return (walk.Count(x => x == "n") == walk.Count(x => x == "s")) && (walk.Count(x => x == "w") == walk.Count(x => x == "e")) && walk.Length == 10;
+    }
+}
