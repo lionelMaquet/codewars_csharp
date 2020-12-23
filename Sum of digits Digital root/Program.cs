4fb7cbb578ca944172dd0805711bee0f5c8b0f28
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-23 15:45:20 +0100

    sum of digits / digital root

---
+++ Sum of digits Digital root/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sum_of_digits_Digital_root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
