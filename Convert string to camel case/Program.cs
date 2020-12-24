32e1fdfd5a079952e1ce847325ab7f31bbd10970
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-24 11:21:24 +0100

    convert string to camel case done

---
+++ Convert string to camel case/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string ToCamelCase(string str)
+    {
+        string[] substrings = str.Split('_', '-');
+        string res = substrings[0];
+        for (int i = 1; i < substrings.Length; i++)
+        {
+            res += char.ToUpper(substrings[i].First()) + substrings[i].Substring(1);
+        }
+        return res;
+    }
+}
