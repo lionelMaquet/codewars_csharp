837623146617c150a64025bde0b0617ea0217bf0
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-25 15:55:59 +0100

    create new kata

---
+++ Delete occurrences of an element if it occurs more than n times/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// link : https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/csharp
+
+namespace Delete_occurrences_of_an_element_if_it_occurs_more_than_n_times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
