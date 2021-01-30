86d4d38efd131294fa45ede1780f87a98a831061
Lionel Maquet <lionel.maquet@gmail.com>
2021-01-30 20:22:23 +0100

    create Premutations

---
+++ Permutations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// link to kata : https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/csharp
+
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<string> SinglePermutations(string s)
+        {
+
+            // Your code here!
+            return null;
+        }
+    }
+}
