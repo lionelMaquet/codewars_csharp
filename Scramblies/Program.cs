64c0cd9779a2053e357fae771693ce4f91826de3
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-05 18:03:49 +0100

    Scramblies done

---
+++ Scramblies/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scramblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool Scramble(string str1, string str2)
+        {
+
+            List<char> chars = new List<char>();
+            chars.AddRange(str1);
+            
+            foreach(char c in str2)
+            {
+                if (chars.Contains(c))
+                {
+                    chars.Remove(c);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
