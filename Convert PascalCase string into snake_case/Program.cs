b2dd74b4c19524cb83dfc034f26403f6476087f1
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-07 19:29:44 +0100

    public static string ToUnderscore(int str)

---
+++ Convert PascalCase string into snake_case/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Convert_PascalCase_string_into_snake_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToUnderscore(56));
+            Console.WriteLine(ToUnderscore("AlphaBet"));
+        }
+
+        public static string ToUnderscore(int str)
+        {
+            return str.ToString();
+        }
+
+        public static string ToUnderscore(string str)
+        {
+            List<char> underscored = new List<char>();
+
+            foreach(char c in str)
+            {
+                if (Char.IsUpper(c) && !Char.IsDigit(c))
+                {
+                    underscored.Add('_');
+                }
+                
+                underscored.Add(Char.ToLower(c));
+            }
+
+            return new string(underscored.ToArray()).Remove(0, 1);
+        }
+    }
+}
