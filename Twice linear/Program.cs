77fef939ac7e8cd7e8b4bcc5fa614ecd0c27cb16
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-08 14:32:44 +0100

    Twice Linear - execution timed out

---
+++ Twice linear/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twice_linear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DblLinear(50));
+        }
+
+        public static int DblLinear(int n)
+        {
+            List<int> dblLinearList = new List<int>() { 1 };
+
+            while (dblLinearList.Count < n + 1)
+            {
+                dblLinearList.Add(NextDblLinear(dblLinearList));
+            }
+
+            dblLinearList.Sort();
+            return dblLinearList[n];
+        }
+
+        public static int NextDblLinear(List<int> p)
+        {
+            List<int> TempList = new List<int>();
+            foreach (int o in p)
+            {
+                int x = 2 * o + 1;
+                int y = 3 * o + 1;
+                new List<int>() { x, y }.ForEach(el => { if (!p.Contains(el)) { TempList.Add(el); } });
+            }
+            TempList.Sort();
+            return TempList[0];
+        }
+    }
+}
