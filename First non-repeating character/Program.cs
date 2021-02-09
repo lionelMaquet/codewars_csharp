05537d4c65d9813733e59728205451428705e298
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-09 20:47:06 +0100

    First non-repeating character done

---
+++ First non-repeating character/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace First_non_repeating_character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (!s.Remove(i, 1).ToLower().Contains(char.ToLower(s[i])))
+                {
+                    return s[i].ToString();
+                }
+            }
+            return "";
+        }
+    }
+}
