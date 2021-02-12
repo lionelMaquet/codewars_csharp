7eabe75764964d50ec9cdcbb7e45152d6fb241e4
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-12 19:57:37 +0100

    Whats a perfect power anyway timed out

---
+++ What's a Perfect Power anyway/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace What_s_a_Perfect_Power_anyway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static (int, int)? IsPerfectPower(int n)
+        {
+            return null;
+        }
+    }
+}
