be43d179aa18b55d162d9a61e87efd09f52a9a96
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-18 19:32:08 +0100

    Spoonerize Me failing for some tests, possible error in website

---
+++ Spoonerize Me/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Spoonerize_Me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Spoonerize(string str)
+        {
+            string[] splittedStr = str.Split(' ');
+
+            string spoonerizedWordOne = string.Concat(splittedStr[1].Substring(0, 2), splittedStr[0].Substring(2));
+            string spoonerizedWordTwo = string.Concat(splittedStr[0].Substring(0, 2), splittedStr[1].Substring(2));
+
+            return string.Concat(spoonerizedWordOne, " ", spoonerizedWordTwo);
+        }
+    }
+}
