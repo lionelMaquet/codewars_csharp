a906eb31b800b6d94cba78f23b3ef3b2a144af52
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-19 19:26:33 +0100

    Transportation on vacation done

---
+++ Transportation on vacation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Transportation_on_vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int RentalCarCost(int d)
+        {
+            int totalWithoutOffer = d * 40;
+             
+            if (d >= 3)
+            {
+                int totalWithOffer = d >= 7 ? totalWithoutOffer - 50 : totalWithoutOffer - 20;
+                return totalWithOffer;
+            }
+
+            return totalWithoutOffer;
+        }
+    }
+}
