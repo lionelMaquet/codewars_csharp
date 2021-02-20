703d9cbbf7d41780b4664af764f8010bc22fe0e4
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-20 16:37:40 +0100

    Century From Year done

---
+++ Century From Year/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Century_From_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
