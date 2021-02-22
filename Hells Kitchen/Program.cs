ed7e63ba2c8dc04d54792e2d5f7ed06b4e55220f
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-22 16:34:13 +0100

    hells kitchen done

---
+++ Hells Kitchen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hells_Kitchen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Gordon(string a)
+        {
+            return string.Concat(string.Concat(a.Select(e => e == 'a' ? '@' : (new List<char>() { 'e', 'i', 'o', 'u' }.Contains(e) ? '*' : char.ToUpper(e)))).Replace(" ", "!!!! "),"!!!!");
+        }
+    }
+}
