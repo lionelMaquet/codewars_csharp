08d5579d8918ef00183e64b2874bf0ca14e62b47
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-24 22:25:55 +0100

    Odd or Even done

---
+++ Odd Or Even/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Odd_Or_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string OddOrEven(int[] array)
+        {
+            return array.ToList().Sum() % 2 == 0 ? "even" : "odd";
+        }
+    }
+}
