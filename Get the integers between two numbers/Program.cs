8a89dc87460bcfd8042709757a635a91aadaa8ea
Lionel Maquet <lionel.maquet@gmail.com>
2021-02-28 18:58:56 +0100

    get the integers between two numbers done

---
+++ Get the integers between two numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Get_the_integers_between_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] Range(int startNum, int endNum)
+        {
+            return endNum - startNum > 0 ? Enumerable.Range(startNum + 1, endNum - startNum - 1).ToArray() : new int[] { };
+        }
+    }
+}
