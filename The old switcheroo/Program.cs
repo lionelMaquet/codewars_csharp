155a16a8d1d1cf94036caffb43dc7fc62eefc804
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-01 16:28:30 +0100

    The old switcheroo done

---
+++ The old switcheroo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace The_old_switcheroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Vowel2Index(string str)
+        {
+            List<string> switcheroo = new List<string>();
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                string c = new List<char>() { 'a', 'e', 'i', 'o', 'u' }.Contains(str[i]) ? (i + 1).ToString() : str[i].ToString();
+                switcheroo.Add(c);
+            }
+
+            return String.Join("", switcheroo.ToArray());
+        }
+    }
+}
