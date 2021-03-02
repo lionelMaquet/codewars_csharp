65b5e0ce8b8443a71003f7b76a6c675d8e01b7d2
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-02 20:09:20 +0100

    Return Negative done

---
+++ Return Negative/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Return_Negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int MakeNegative(int number)
+        {
+            return number > 0 ? -number : number;
+        }
+    }
+}
