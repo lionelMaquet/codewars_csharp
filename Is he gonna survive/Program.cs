6a8b9103931b91dc5ad3efea67713cc02a3b78ce
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-03 21:03:53 +0100

    is he gonna survive done

---
+++ Is he gonna survive/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Is_he_gonna_survive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool Hero(int bullets, int dragons)
+        {
+            return bullets >= dragons * 2;
+        }
+    }
+}
