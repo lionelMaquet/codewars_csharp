da004203ed99c55c2e313374db169ac0e4d6a28d
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-04 17:23:27 +0100

    Reversed Strings

---
+++ Reversed Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Reversed_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Solution(string str)
+        {
+            char[] strToArray = str.ToCharArray();
+            Array.Reverse(strToArray);
+            return new string(strToArray);
+        }
+    }
+}
