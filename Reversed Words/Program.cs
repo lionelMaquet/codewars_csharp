75cbec94c79440caa13b3811cf846d71ebd65b14
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-05 18:18:50 +0100

    Reversed Words done

---
+++ Reversed Words/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Reversed_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string ReverseWords(string str)
+        {
+            return String.Join(' ', str.Split(' ').Reverse());
+        }
+    }
+}
