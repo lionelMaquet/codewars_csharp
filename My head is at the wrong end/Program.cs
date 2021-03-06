9c693a29be011d4a86823315aae69be4c64c1e8b
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-06 10:09:06 +0100

    My head is at the wrong end done

---
+++ My head is at the wrong end/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace My_head_is_at_the_wrong_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string[] FixTheMeerkat(string[] arr)
+        {
+            return arr.Reverse().ToArray();
+        }
+    }
+}
