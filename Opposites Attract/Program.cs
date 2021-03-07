93f4ee494490d0f1183f2f84cb787013222a00a8
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-07 20:15:15 +0100

    Opposites attract done

---
+++ Opposites Attract/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Opposites_Attract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool lovefunc(int flower1, int flower2)
+        {
+            return (flower1 + flower2) % 2 == 1;
+        }
+    }
+}
