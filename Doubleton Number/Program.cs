731cb93a5ee1c407dd0ee73941eb69ba69fd7ea3
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-08 19:16:56 +0100

    Doubleton Number done

---
+++ Doubleton Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Doubleton_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Doubleton(int num)
+        {
+            int next = num + 1;
+            while(next.ToString().Distinct().Count() != 2)
+            {
+                next++;
+            }
+            return next;
+        }
+    }
+}
