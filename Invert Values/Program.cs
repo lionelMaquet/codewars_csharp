0e21787d5aa8fdc3194090200bbf75927f3bd249
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-12 17:57:32 +0100

    Invert Values done

---
+++ Invert Values/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Invert_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvertValues(new int[] { 1, 2, 3 });
+        }
+
+        public static int[] InvertValues(int[] input)
+        {
+            return input.Select(s => s * -1).ToArray();
+        }
+    }
+}
