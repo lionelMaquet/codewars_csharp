b6937fc02e3b6645667521f83e140a3ca21106dc
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-13 13:19:50 +0100

    Sum of positive done

---
+++ Sum Of Positive/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Of_Positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int PositiveSum(int[] arr)
+        {
+            return arr.Select(e => e > 0 ? e : 0).Sum();
+        }
+    }
+}
