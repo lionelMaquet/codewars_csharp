ab8f85b7a3e63cf5776e4595ce13ba13c983299f
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-15 18:05:17 +0100

    Sums of parts done

---
+++ Sums of parts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sums_of_parts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] PartsSums(int[] ls)
+        {
+            int total = ls.ToList().Sum();
+            List<int> partsSums = new List<int>() { total };
+            for (int i = 0; i < ls.Count(); i++)
+            {
+                partsSums.Add(total - ls[i]);
+                total -= ls[i];
+            }
+
+            return partsSums.ToArray();
+        }
+    }
+}
