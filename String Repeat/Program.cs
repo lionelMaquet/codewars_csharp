590fc4b4513206bb8d4066c436c4a40f39d7b720
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-16 16:24:54 +0100

    String Repeat done

---
+++ String Repeat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace String_Repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string RepeatStr(int n, string s)
+        {
+            return new String('X', n).Replace("X", s);
+        }
+    }
+}
