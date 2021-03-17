789fcfe0932c5f2c47ab9d7bd3120355581bb48e
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-17 17:38:25 +0100

    MakeUpperCase done

---
+++ MakeUpperCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MakeUpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+}
