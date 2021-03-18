f8bfc5397b6634b07188eb694322c2fdc8029c90
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-18 18:07:58 +0100

    Twice as old done

---
+++ Twice as old/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Twice_as_old
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            return Math.Abs(dadYears - sonYears * 2);
+        }
+    }
+}
