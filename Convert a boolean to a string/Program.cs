25f9d38540cc2bd1920baaf969458b9936ae38ca
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-19 14:30:09 +0100

    Convert a boolean to a string done

---
+++ Convert a boolean to a string/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Convert_a_boolean_to_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string boolean_to_string(bool b)
+        {
+            return b ? "True" : "False";
+        }
+    }
+}
