b0d6f3f48db74014e0b97dda939f4ac50553995b
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-20 17:03:13 +0100

    Filter out the geese done

---
+++ Filter out the geese/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Filter_out_the_geese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static IEnumerable<string> GooseFilter(IEnumerable<string> birds)
+        {
+            string[] geese = new string[] { "African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher" };
+            return birds.Where(e => !geese.Contains(e)).ToList();
+        }
+    }
+}
