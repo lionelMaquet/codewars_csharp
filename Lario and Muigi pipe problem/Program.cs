45164844c7e6b89f0fb003e38a0a1637e80232e7
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-21 20:46:17 +0100

    Lario and Muigi pipe problem done

---
+++ Lario and Muigi pipe problem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lario_and_Muigi_pipe_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static List<int> PipeFix(List<int> numbers)
+        {
+            return new List<int>(Enumerable.Range(numbers[0], numbers.Last() - numbers[0] + 1));
+        }
+    }
+}
