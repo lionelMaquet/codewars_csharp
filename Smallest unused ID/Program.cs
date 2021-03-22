57eeb02a8d24805e065f572aa5896b65abdb025f
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-22 18:31:44 +0100

    Smallest unused id done

---
+++ Smallest unused ID/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Smallest_unused_ID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int NextId(int[] ids)
+        {
+            for (int check = 0;; check++)
+            {
+                if (!ids.ToList().Contains(check))
+                {
+                    return check;
+                }
+            }
+        }
+    }
+}
