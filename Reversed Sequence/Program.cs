fc2b8f23c38723fd9ebd516abffa8f29c43af785
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-23 21:20:13 +0100

    Reversed Sequence done

---
+++ Reversed Sequence/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reversed_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] ReverseSeq(int n)
+        {
+            return Enumerable.Range(1, n).Reverse().ToArray();
+        }
+    }
+}
