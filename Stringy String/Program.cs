542c6a93c2b42f66eb39d625568fc013f28af746
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-24 21:26:44 +0100

    Stringy string done

---
+++ Stringy String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Stringy_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Stringy(int size)
+        {
+            return string.Concat(new string('1', size).Select((e,i) => i % 2 == 1 ? '0' : '1'));
+        }
+    }
+}
