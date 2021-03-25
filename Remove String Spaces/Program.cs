87387a164896e5bd5ce3c5c7d8f0b7b0bc789abf
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-25 20:15:19 +0100

    Remove String Spaces done

---
+++ Remove String Spaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Remove_String_Spaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string NoSpace(string input)
+        {
+            return input.Replace( " ", "" );
+        }
+    }
+}
