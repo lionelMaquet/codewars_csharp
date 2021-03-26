12a8c8f937387c6951435dec9ffc0c04418f9e26
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-26 19:56:13 +0100

    Find the smallest integer in the array done

---
+++ Find the smallest integer in the array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Find_the_smallest_integer_in_the_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int FindSmallestInt(int[] args)
+        {
+            return args.Min();
+        }
+    }
+}
