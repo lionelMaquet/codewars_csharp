a1ed8209bd0aa2d7e7af4ce4ada0fd570d96e59d
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-27 14:27:08 +0100

    Is there a vowel in there done

---
+++ Is there a vowel in there/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Is_there_a_vowel_in_there
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsVow(new object[] { 118, 117, 120, 121, 117, 98, 122, 97, 120, 106, 104, 116, 113, 114, 113, 120, 106 });
+        }
+
+        public static object[] IsVow(object[] a)
+        {
+            return a.Select(e => new List<char>() { 'a', 'e', 'i', 'o', 'u' }.Contains(Convert.ToChar(e)) ? Convert.ToChar(e).ToString() : e).ToArray<object>();
+        }
+    }
+}
