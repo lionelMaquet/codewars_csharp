695bd70c0bf40d705bcf6b514b31f9fc3bb2411f
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-28 19:11:44 +0200

    is this my tail done

---
+++ Is this my tail/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Is_this_my_tail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool CorrectTail(string body, string tail)
+        {
+            return tail[0] == body[body.Length - 1];
+        }
+    }
+}
