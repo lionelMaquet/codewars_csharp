3994d9e3024d4b54c8ab161ee364fdf8dc0302a0
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-29 20:38:25 +0200

    Remove exclamation marks done

---
+++ Remove exclamation marks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Remove_exclamation_marks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string RemoveExclamationMarks(string s)
+        {
+            return s.Replace("!", "");
+        }
+    }
+}
