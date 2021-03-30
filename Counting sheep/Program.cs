b996062a71d00cf1bbdd80e6c2b7e460fa8794b6
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-30 18:51:47 +0200

    counting sheep done

---
+++ Counting sheep/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Counting_sheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int CountSheeps(bool[] sheeps)
+        {
+            return sheeps.ToList().Count(e => e); // same as writing e => e == true
+        }
+    }
+}
