923d1a4db9fd3e5fe7ee41a407e6808b2bcae3d7
Lionel Maquet <lionel.maquet@gmail.com>
2021-03-31 20:03:48 +0200

    cat years dog years done

---
+++ Cat years Dog years/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cat_years_Dog_years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] humanYearsCatYearsDogYears(int humanYears)
+        {
+            int catYears = humanYears > 2 ? 24 + ((humanYears - 2) * 4) : (humanYears == 1 ? 15 : 24);
+            int dogYears = humanYears > 2 ? 24 + ((humanYears - 2) * 5) : (humanYears == 1 ? 15 : 24);
+            return new int[] { humanYears, catYears, dogYears };
+        }
+    }
+}
