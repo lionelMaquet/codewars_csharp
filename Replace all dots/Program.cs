8286f2465e219600a439d41251a92da6b56aa530
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-01 20:07:04 +0200

    Replace all dots done

---
+++ Replace all dots/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Replace_all_dots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string ReplaceDots(string str)
+        {
+            return str.Replace('.', '-');
+        }
+    }
+}
