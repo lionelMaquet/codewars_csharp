4d43a6cea44c8d46e23e2a47781c369065b8757b
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-02 23:49:13 +0200

    the wide mouthed frog done

---
+++ The wide mouthed frog/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace The_wide_mouthed_frog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string MouthSize(string animal)
+        {
+            return animal.ToLower() == "alligator" ? "small" : "wide";
+        }
+    }
+}
