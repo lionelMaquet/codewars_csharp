db7de0e410a1e7a7f88123d9212b0f9ba4c0b4dc
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-03 21:31:09 +0200

    DNA to RNA conversion done

---
+++ DNA to RNA conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace DNA_to_RNA_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string dnaToRna(string dna)
+        {
+            return string.Concat(dna.Select(e => e == 'T' ? 'U' : e));
+        }
+    }
+}
