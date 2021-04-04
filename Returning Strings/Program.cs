3e2a3ef612fbcc9fcafc576fa3bc7c50fad4f0b4
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-04 09:52:00 +0200

    Returning strings done

---
+++ Returning Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Returning_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Greet(string name)
+        {
+            return $"Hello, {name} how are you doing today?";
+        }
+    }
+}
