834a10728207d6125d83763b58c271b1b4b5d8af
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-05 20:17:37 +0200

    Generate range of integers done

---
+++ Generate range of integers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Generate_range_of_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateRange(2, 10, 2);
+        }
+        
+        public static int[] GenerateRange(int min, int max, int step)
+        {
+            return Enumerable.Range(min, max - min + 1).Select(e => e == min || (e - min) % step == 0 ? e : -1).Where(i => i != -1).ToArray();
+        }
+    }
+}
