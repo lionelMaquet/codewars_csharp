6429a6965ea675a8a8d7c27458f95ef1f9d47556
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-06 19:36:24 +0200

    what is between done

---
+++ What is between/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace What_is_between
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] Between(int a, int b)
+        {
+            return Enumerable.Range(a, b - a + 1).ToArray();
+        }
+    }
+}
