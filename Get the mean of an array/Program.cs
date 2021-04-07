1c45c08862907a8ac50ee92ffde6ba3598c30370
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-07 14:06:49 +0200

    get the mean of an array done

---
+++ Get the mean of an array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Get_the_mean_of_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int GetAverage(int[] marks)
+        {
+            return marks.Sum() / marks.Length;
+        }
+    }
+}
