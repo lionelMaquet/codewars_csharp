4be3c729fdc23c49bb115b4be9ba9bf5a1910825
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-08 14:56:11 +0200

    are arrow functions odd

---
+++ Are arrow functions odd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Are_arrow_functions_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<int> Odds(List<int> values) =>
+    // arrow it
+    values.Where( e => e % 2 == 1 )
+          .ToList();
+    }
+}
