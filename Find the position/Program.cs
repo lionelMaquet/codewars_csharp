87706822499b1afb45a7e5c0766ef4234007637a
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-09 19:27:22 +0200

    Find the position done

---
+++ Find the position/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Find_the_position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Position(char alphabet)
+        {
+            return $"Position of alphabet: {"abcdefghijklmnopqrstuvwxyz".IndexOf(alphabet) + 1}"; 
+        }
+    }
+}
