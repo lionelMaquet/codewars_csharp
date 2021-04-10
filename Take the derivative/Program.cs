078d15d63047fffb6894a5b439750a3b8785c86b
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-10 12:06:15 +0200

    take the derivative done

---
+++ Take the derivative/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Take_the_derivative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Derive(double coefficient, double exponent)
+        {
+            return $"{coefficient * exponent}x^{exponent - 1}";
+        }
+    }
+}
