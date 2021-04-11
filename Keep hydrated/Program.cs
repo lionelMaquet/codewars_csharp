82a77e7bece200a5bdb6e7d0dc9b2dc78a3e0de3
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-11 23:22:59 +0200

    keep hydrated done

---
+++ Keep hydrated/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Keep_hydrated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Litres(double time)
+        {
+            return (int)Math.Floor(time / 2);
+        }
+    }
+}
