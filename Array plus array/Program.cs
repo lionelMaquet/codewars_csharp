237537602bdf21170c12d64ab838c89ccd8bae68
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-13 16:07:32 +0200

    Array plus array done

---
+++ Array plus array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_plus_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int ArrayPlusArray(int[] arr1, int[] arr2)
+        {
+            return arr1.Sum() + arr2.Sum();
+        }
+    }
+}
