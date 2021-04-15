3bbcc8a7cb8943cda76bf7baaaf171ab2778d6d5
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-15 21:48:57 +0200

    I love you a little ... done

---
+++ I love you, a little , a lot, passionately not at all/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace I_love_you__a_little___a_lot__passionately_not_at_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string HowMuchILoveYou(int nb_petals)
+        {
+            return new List<string> { "I love you", "a little", "a lot", "passionately", "madly", "not at all"}[(nb_petals - 1) % 6];
+        }
+    }
+}
