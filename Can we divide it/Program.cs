608cd9d4c9f121aef40c88d9209da60ea9e13c4a
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-16 20:00:02 +0200

    Can we divide it done

---
+++ Can we divide it/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Can_we_divide_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool is_divide_by(int number, int a, int b)
+        {
+            return (number % a == 0 && number % b == 0);
+        }
+    }
+}
