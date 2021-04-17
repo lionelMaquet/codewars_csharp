edcbcdfb060ea5a4768827a39d479b21982a3ce7
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-17 22:26:52 +0200

    Abbreviate a two word name in progress

---
+++ Abbreviate a two word name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Abbreviate_a_two_word_name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string AbbrevName(string name)
+        {
+            List<char> chars = new List<char>();
+            name.foreach(char c){
+                if (char.ToUpper(c) == c) {
+                    chars.add(c);
+                }
+            }
+        }
+    }
+}
