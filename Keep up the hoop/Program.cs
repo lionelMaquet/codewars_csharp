fde730b2679b0ee42c73677ade44de144a6b376d
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-18 23:46:47 +0200

    Keep up the hoop done

---
+++ Keep up the hoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Keep_up_the_hoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string HoopCount(int n)
+        {
+            return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+        }
+    }
+}
