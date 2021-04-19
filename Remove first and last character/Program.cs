3f2599686e96da4c43853248882ac5c96ea1c993
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-19 21:43:11 +0200

    Remove first and last character done

---
+++ Remove first and last character/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Remove_first_and_last_character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Remove_char(string s)
+        {
+            return s.Substring(1, s.Length - 2);
+        }
+    }
+}
