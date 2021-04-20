86899cb8d85e0c4350082cd2d9bcd9031ac0a9c8
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-20 20:37:15 +0200

    Price of mangoes done

---
+++ Price of mangoes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Price_of_mangoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Mango(int quantity, int price)
+        {
+            return (((quantity - quantity % 3) * 2 / 3) + quantity % 3) * price;
+        }
+    }
+}
