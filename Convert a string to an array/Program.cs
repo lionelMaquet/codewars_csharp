6775f876d0816e4808084fe6be9e14ed6efc425d
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-21 22:04:31 +0200

    Convert a string to an array done

---
+++ Convert a string to an array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Convert_a_string_to_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string[] StringToArray(string str)
+        {
+            return str.Split(' ');
+        }
+    }
+}
