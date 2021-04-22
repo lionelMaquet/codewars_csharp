38a515ca868e7cf8301e85d8d2e5153b03b41e66
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-22 20:21:39 +0200

    THe feast of many beasts done

---
+++ The feast of many beasts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace The_feast_of_many_beasts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool Feast(string beast, string dish)
+        {
+            return beast[0] == dish[0] && beast[beast.Length - 1] == dish[dish.Length - 1];
+        }
+    }
+}
