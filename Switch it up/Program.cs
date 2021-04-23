efe8cdabf35496b9cfe1c5630af8fc68d5f2b08c
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-23 17:54:37 +0200

    Switch it up done

---
+++ Switch it up/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Switch_it_up
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string SwitchItUp(int number)
+        {
+            return "Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine".Split(',')[number]; 
+        }
+    }
+}
