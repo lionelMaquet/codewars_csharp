9a65780d616afc4ec8a018aa216ec0fb9a112090
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-24 18:45:16 +0200

    Are you playing banjo done

---
+++ Are you playing banjo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Are_you_playing_banjo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string AreYouPlayingBanjo(string name)
+        {
+            return char.ToLower(name[0]) == 'r' ? $"{name} plays banjo" : $"{name} does not play banjo";
+        }
+    }
+}
