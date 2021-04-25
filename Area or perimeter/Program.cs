cdeaba5441d214782963c8b1b3ff2ddf727f5d19
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-25 21:16:29 +0200

    area or perimeter done

---
+++ Area or perimeter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Area_or_perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int AreaOrPerimeter(int l, int w)
+        {
+            return l == w ? l * w : (l * 2) + (w * 2);
+        }
+    }
+}
