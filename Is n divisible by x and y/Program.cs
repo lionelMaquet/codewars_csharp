d7dd9d1a762e3a6b40ad6ace7bc43c0e6b086109
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-26 19:24:14 +0200

    is n divisible by x and y done

---
+++ Is n divisible by x and y/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Is_n_divisible_by_x_and_y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool isDivisible(long n, long x, long y)
+        {
+            return n % x == 0 && n % y == 0;
+        }
+    }
+}
