ef842faf82e9d7e0b5472f5a14f8d4d623209053
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-27 21:08:26 +0200

    convert a number to a string done

---
+++ Convert a number to a string/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Convert_a_number_to_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string NumberToString(int num)
+        {
+            return num.ToString();
+        }
+    }
+}
