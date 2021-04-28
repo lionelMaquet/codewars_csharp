4fe4172f1f31a7d838e020d8f152c220458b9f50
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-28 22:08:05 +0200

    How many lightsabers do you own done

---
+++ How many lightsabers do you own/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace How_many_lightsabers_do_you_own
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int HowManyLightsabersDoYouOwn(string name)
+        {
+            return name == "Zach" ? 18 : 0;
+        }
+    }
+}
