88aeffba6d3b9d96d98b60a3d7324d9bc08b4344
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-29 21:45:58 +0200

    will there be enough space done

---
+++ Will there be enough space/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Will_there_be_enough_space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Enough(int cap, int on, int wait)
+        {
+            return cap >= on + wait ? 0 : Math.Abs(cap - (on + wait)) ; 
+        }
+    }
+}
