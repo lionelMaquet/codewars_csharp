607e38bb372e98fa24d4c77da8976e77a0d72b27
Lionel Maquet <lionel.maquet@gmail.com>
2021-04-30 21:27:17 +0200

    Do you speak English done

---
+++ Do you speak English/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Do_you_speak_English
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool SpeakEnglish(string sentence)
+        {
+            return sentence.ToLower().Contains("english");
+        }
+    }
+}
