d0d0658e50e0c2632ffb9f21fcbb11f792224f0c
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-01 22:07:44 +0200

    convert boolean values to strings yes or no done

---
+++ Convert boolean values to strings Yes or No/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Convert_boolean_values_to_strings_Yes_or_No
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string boolToWord(bool word)
+        {
+            return word ? "Yes" : "No";
+        }
+    }
+}
