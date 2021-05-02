47d5de1957f9b9c298786d384777133f6a20682c
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-02 14:59:52 +0200

    calculate average done

---
+++ Calculate average/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Calculate_average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static double FindAverage(double[] array)
+        {
+            return array.Length > 0 ? array.ToList().Average() : 0;
+        }
+    }
+}
