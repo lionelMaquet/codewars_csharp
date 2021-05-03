590b92c709953fd648a4c9d7d9815da0b8bb4a46
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-03 21:50:38 +0200

    short long short done

---
+++ Short long short/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Short_long_short
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Solution(string a, string b)
+        {
+            return a.Length > b.Length ? string.Concat(b, a, b) : string.Concat(a, b, a);
+        }
+    }
+}
