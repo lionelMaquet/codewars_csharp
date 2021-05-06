1f24e461e886b51b6a08fb8d8bac5f9557a6b770
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-06 21:01:59 +0200

    Even or Odd done

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string EvenOrOdd(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
