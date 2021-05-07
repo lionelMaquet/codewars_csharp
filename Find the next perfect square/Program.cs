c7d7973a4740a9f15359a682b43324b7de0dd3a7
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-07 23:10:30 +0200

    Find the next perfect square done

---
+++ Find the next perfect square/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Find_the_next_perfect_square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static long FindNextSquare(long num)
+        {
+            return Math.Sqrt((double)num) == (int)Math.Sqrt((double)num) ? (long)Math.Pow(Math.Sqrt(num) + 1, 2) : -1;
+        }
+    } 
+}
