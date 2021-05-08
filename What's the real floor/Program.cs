8faaf6297641e17e3f8355171ad4cf45a82b2e32
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-08 18:16:22 +0200

    whats the real floor done

---
+++ What's the real floor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace What_s_the_real_floor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Whats the real floor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Whats_the_real_floor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int GetRealFloor(int n)
+        {
+            return n <= 0 ? n : (n >= 13 ? n - 2 : n - 1);
+        }
+    }
+}
