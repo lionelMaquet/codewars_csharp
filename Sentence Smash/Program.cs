ae7db0d534f6ad3e06dbb97d0e89cf0ed694712b
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-10 19:35:36 +0200

    Sentence smash done

---
+++ Sentence Smash/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sentence_Smash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Smash(string[] words)
+        {
+            string smashed = "";
+
+            foreach(string s in words)
+            {
+                smashed = string.Concat(smashed, " ");
+                smashed = string.Concat(smashed, s);
+            }
+
+            return smashed.Remove(0,1);
+        }
+    }
+}
