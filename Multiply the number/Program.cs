cbd0958d5b492dfa5fe34bd4e119a91995923dbe
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-11 23:30:01 +0200

    multiply the number done

---
+++ Multiply the number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Multiply_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Multiply(int number)
+        {
+            return (int)(number * (Math.Pow(5, Math.Abs(number).ToString().Length)));
+        }
+    }
+}
