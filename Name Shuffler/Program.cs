b51717c6a17a096cc876b5086647276a2b53f15b
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-12 17:04:56 +0200

    Name Shuffler done

---
+++ Name Shuffler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Name_Shuffler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string NameShuffler(string str)
+        {
+            return string.Concat(str.Split(" ")[1], " ", str.Split(" ")[0]); 
+        }
+    }
+}
