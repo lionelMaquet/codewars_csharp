ec1744d508cbdcf0d38e796785cba574b24e46de
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-14 23:31:59 +0200

    Vowel Count done

---
+++ Vowel Count/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Vowel_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int GetVowelCount(string str)
+        {
+            return str.Count(i => "aeiou".Contains(i));
+        }
+    }
+}
