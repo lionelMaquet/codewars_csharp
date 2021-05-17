6b9b98a51d6e6738e20ebdc40b237f7b0748fdbe
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-17 21:42:07 +0200

    How good are you really ? done

---
+++ How good are you really/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace How_good_are_you_really
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            return ClassPoints.ToList().Average() < YourPoints ? true : false;
+        }
+    }
+}
