6730701d0a487049f42ed90c5513c24195b343e1
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-21 21:36:41 +0200

    Drink about done

---
+++ Drink About/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Drink_About
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string PeopleWithAgeDrink(int old)
+        {
+            return old < 14 ? "drink toddy" : (old < 18 ? "drink coke" : (old < 21 ? "drink beer" : "drink whisky"));
+        }
+    }
+}
