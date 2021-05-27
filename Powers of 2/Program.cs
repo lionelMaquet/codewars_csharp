134c0860f5db8b155b2edce532d3d49a8ac55025
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-27 21:03:27 +0200

    powers of 2 done

---
+++ Powers of 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static BigInteger[] PowersOfTwo(int n)
+        {
+            List<BigInteger> powers = new List<BigInteger>();
+
+            for (int i = 0; i <= n; i++)
+            {
+                powers.Add((BigInteger)Math.Pow(2, i));
+            }
+            
+            return powers.ToArray();
+        }
+    }
+}
