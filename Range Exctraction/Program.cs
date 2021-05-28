b8618dbd23f23b83e98f290e1bd52b9dba744604
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-28 21:32:09 +0200

    ...

--- Range Exctraction/Program.cs
+++ Range Exctraction/Program.cs
@@ -8,7 +8,6 @@ namespace Range_Exctraction
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine( Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 }));
         }
 
