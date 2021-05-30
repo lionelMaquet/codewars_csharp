52c6fd453025d9fe7a6ee677319c3964a68441a4
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-30 22:41:31 +0200

    parse nice int from char done

---
+++ Parse nice int from char/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parse_nice_int_from_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int GetAge(string inputString)
+        {
+            return int.Parse(inputString.Substring(0, 1));
+        }
+    }
+}
