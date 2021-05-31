0e67382bc3f6517f568e426344aa6c711780857a
Lionel Maquet <lionel.maquet@gmail.com>
2021-05-31 22:55:45 +0200

    watermelon done

---
+++ Watermelon/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool Divide(int weight)
+        {
+            return weight <= 2 ? false : weight % 2 == 0;  
+        }
+    }
+}
