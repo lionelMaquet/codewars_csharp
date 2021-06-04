e6b95aa0c1cab715aa89fe7a87267f8eca2604e3
Lionel Maquet <lionel.maquet@gmail.com>
2021-06-04 22:14:54 +0200

    count by x done

---
+++ Count by X/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_by_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] CountBy(int x, int n)
+        {
+            List<int> ints = new List<int>();
+
+            int i = x;
+
+            while(ints.Count != n)
+            {
+                if (i % x == 0)
+                {
+                    ints.Add(i);
+                }
+                i++;
+            }
+
+            return ints.ToArray();
+        }
+    }
+}
